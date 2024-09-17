99aab286c8fcfdc405306b3ebd141285e77cebb7
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-09-17 14:41:51 -0400

    Add basic implementation of models in airport/models.py

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,126 @@
 from django.db import models
 
-# Create your models here.
+from airport_api_service import settings
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        on_delete=models.CASCADE,
+        related_name="airplanes",
+    )
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    city = models.CharField(max_length=255)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="cities",
+    )
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name="airports",
+    )
+
+    def __str__(self):
+        return f"{self.name}, Location: {self.location}"
+
+
+class Route(models.Model):
+    origin = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes",
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes",
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return (f"{self.origin} -> {self.destination}, "
+                f"Distance: {self.distance}")
+
+
+class Flight(models.Model):
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    crew = models.ManyToManyField(Crew, related_name="flights")
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights",
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"{self.route.origin.location}: {self.departure_time} -> "
+                f"{self.route.destination.location}: {self.arrival_time}, "
+                f"Airplane: {self.airplane}")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+    )
