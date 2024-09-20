6be1acb02ebc951856647fb8a49657376ab907cd
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-09-20 12:28:17 -0400

    Add permission, authentication, and pagination classes to REST_FRAMEWORK in settings.py

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "airport",
     "user",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10
 }
