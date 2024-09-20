98a9e329d697a0ecc442e6d29d1ef26874fdf7d3
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-09-20 12:26:03 -0400

    Add api/user/ path to airport_api_service/urls.py

--- airport_api_service/urls.py
+++ airport_api_service/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airports/", include("airport.urls", namespace="airport")),
+    path("api/user/", include("user.urls", namespace="user")),
 ] + debug_toolbar_urls()
